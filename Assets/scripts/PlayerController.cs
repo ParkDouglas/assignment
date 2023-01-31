c79ab2b4ace32334af834c0115694e294d9d9aba
ParkDouglas <116034374+ParkDouglas@users.noreply.github.com>
2023-01-31 12:54:23 -0500

    lab 3 fixed
    extra two animations stealth and

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -32,25 +32,30 @@ public class PlayerController : MonoBehaviour
         if (speed <= 0)
         {
             speed = 6.0f;
-           
+
+            Debug.Log("speed was set incorrect, defaulting to" + speed.ToString());
+
         }
 
         if (jumpForce <= 0)
         {
-            jumpForce = 300;
-            
+            jumpForce = 400;
+
+            Debug.Log("Jump force was set incorrect, defaulting to" + jumpForce.ToString());
         }
 
         if (groundCheckRadius <= 0)
         {
             groundCheckRadius = 0.2f;
-          
+
+            Debug.Log("Ground check radius was set incorrect, defaulting to" + groundCheckRadius.ToString());
         }
 
         if (!groundCheck)
         {
             groundCheck = GameObject.FindGameObjectWithTag("GroundCheck").transform;
-          
+
+            Debug.Log("Ground check not set, finding it manually!");
         }
     }
 
@@ -58,29 +63,38 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float hinput = Input.GetAxis("Horizontal");
-        float vinput = Input.GetAxisRaw("Verticle");
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundlayer);
-        
-        if(isGrounded && Input.GetButtonDown("Jump"))
+        bool stealth = false;
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.velocity = Vector2.zero;
             rb.AddForce(Vector2.up * jumpForce);
+
+           
         }
 
-        if(Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             anim.SetTrigger("New Trigger");
         }
-        if (vinput < 0)
-        {
-            anim.SetTrigger("stealth");
-        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            stealth = true;
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            stealth = false;
 
         Vector2 moveDirection = new Vector2(hinput * speed, rb.velocity.y);
         rb.velocity = moveDirection;
 
         anim.SetFloat("hinput", Mathf.Abs(hinput));
-        anim.SetBool("isGrounded", isGrounded); 
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("stealth", stealth);
+
+        if (hinput != 0)
+            sr.flipX = (hinput < 0);
+
     }
 }
